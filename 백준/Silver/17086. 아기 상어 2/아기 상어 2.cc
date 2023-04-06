2b4a50b8cb428a4e487616bd934f0eacca2b7f30
Kim Gang San <80745404+River-Mt@users.noreply.github.com>
2023-04-06 17:15:23 +0900

    [Silver II] Title: 아기 상어 2, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.cc
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.cc
@@ -1,83 +1,63 @@
-#include <cstdio>
 #include <iostream>
 #include <algorithm>
 #include <vector>
 #include <queue>
 #include <stack>
-#include <map>
-#include <set>
 #include <string>
-#include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <deque>
+#include <cmath>
 using namespace std;
-#define ll long long
-#define pi pair<int,int>
-#define ppi pair<int,pair<int,int>>
-#define pll pair<ll,ll>
-#define FOR(n) for(int i=0;i<n;++i)
-#define FFOR(n,m) for(int i=0;i<n;++i)for(int j=0;j<m;++j)
-#define FastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define vvi vector<vector<int>>
-#define vi vector<int>
-#define vpi vector<pair<int,int>>
+typedef pair<int, int> pi;
 #define endl '\n'
-#define INF 1e10
-#define MOD 1000000000
-#define MAX 100005
-#define all(v) v.begin(), v.end()
-#define compress(v) v.erase(unique(all(v)),v.end())
-#include <string>
-#include <vector>
-#include <set>
-#include <iostream>
-using namespace std;
-#include <cstdio>
-#include <algorithm>
-#include <vector>
-#include <queue>
-#include <set>
-using namespace std;
-int n,m,ans;
-int arr[55][55], dist[55][55];
-int dr[] = {0,0,-1,1,-1,-1,1,1};
-int dc[] = {-1,1,0,0,-1,1,1,-1};
-queue<pi>q;
+#define MAX 55
+#define FIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-void bfs(){
-    while(!q.empty()){
-        int r = q.front().first;
-        int c = q.front().second;
-        q.pop();
-        ans = max(dist[r][c],ans);
-        for(int i=0;i<8;++i){
-            int nr = r+dr[i];
-            int nc = c+dc[i];
-            if(nr<0||nc<0||nr>=n||nc>=m)continue;
-            if(dist[nr][nc])continue;
-            dist[nr][nc] = dist[r][c]+1;
-            q.push({nr,nc});
-        }
-        
-    }
-}
+int n, m, ans;
+int dr[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+int dc[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+int sea[MAX][MAX]; // 0 : empty, 1 : baby shark
+int dist[MAX][MAX];
+queue<pi> q;
 
-int main(){
-    FastIO
-    cin>>n>>m;
-    for(int i=0;i<n;++i){
-        for(int j=0;j<m;++j){
-            cin>>arr[i][j];
-            if(arr[i][j]){
-                q.push({i,j});
-                dist[i][j]=1;
+void init() {
+    FIO
+    cin >> n >> m;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cin >> sea[i][j];
+            if(sea[i][j]) {
+                dist[i][j] = 1;
+                q.push({i, j});
             }
         }
     }
-    bfs();
-    cout<<ans-1<<endl;
-    
-    return 0;
 }
 
+bool isOverRange(int r, int c) {
+    if(r < 0 || c < 0 || r >= n || c >= m) return true;
+    return false;
+}
+
+void bfs() {
+    while(!q.empty()) {
+        pi now = q.front();
+        q.pop();
+        ans = max(dist[now.first][now.second], ans);
+
+        for(int d = 0; d < 8; d++) {
+            int nr = now.first + dr[d];
+            int nc = now.second + dc[d];
+            if(isOverRange(nr, nc))continue;
+            if(dist[nr][nc])continue;
+            dist[nr][nc] = dist[now.first][now.second] + 1;
+            q.push({nr, nc});
+        }
+    }
+    
+}
+
+int main() {
+    init();
+    bfs();
+    cout << ans - 1;
+    return 0;
+}
