3076a692ab0e815f7cdca5ae00c7bee15d51e989
Kim Gang San <80745404+River-Mt@users.noreply.github.com>
2023-05-13 22:00:16 +0900

    [unrated] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 9.99 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -0,0 +1,12 @@
+def solution(num_list):
+    answer = 0
+    odd_sum = 0
+    even_sum = 0
+    
+    for i in range(len(num_list)):
+        if i % 2: even_sum += num_list[i]
+        else: odd_sum += num_list[i]
+    
+    answer = max(odd_sum, even_sum)
+        
+    return answer
