348c85794f6547d0075e75abb0b5f46928767c1a
Kim Gang San <80745404+River-Mt@users.noreply.github.com>
2023-05-20 18:20:09 +0900

    [unrated] Title: 빈 배열에 추가, 삭제하기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.py
@@ -0,0 +1,14 @@
+from collections import deque
+def solution(arr, flag):
+    answer = deque()
+    
+    for i in range(len(arr)):
+        if flag[i] == True:
+            for _ in range(arr[i] * 2):
+                answer.append(arr[i])
+
+        else :
+            for _ in range(arr[i]):
+                answer.pop()
+    
+    return list(answer)
