bee186721dd845198d8c52950d225bf1a499a449
Kim Gang San <80745404+River-Mt@users.noreply.github.com>
2023-05-21 21:34:42 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.py
@@ -0,0 +1,10 @@
+def solution(id_pw, db):
+    answer = ''
+    for (id, pw) in db:
+        if id_pw[0] == id:
+            if id_pw[1] == pw:
+                return "login"
+            else:
+                return "wrong pw"
+        
+    return "fail"
