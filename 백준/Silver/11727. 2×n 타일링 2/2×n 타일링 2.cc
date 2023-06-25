9c8454f577ecf96b5b497aa42db3b03a2081afb8
Kim Gang San <80745404+River-Mt@users.noreply.github.com>
2023-06-25 13:44:18 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+#define MOD 10007
+int n;
+int dp[1005];
+
+int main() {
+    dp[0] = 1;
+    dp[1] = 1;
+    cin >> n;
+    for(int i=2;i<=n;++i) {
+        dp[i] = (2 * dp[i - 2] + dp[i - 1]) % MOD;
+    }
+    cout << dp[n] << '\n';
+    
+    return 0;
+}
