2db2e4c4a76a90260b1f824906f4bf576261c19e
Kim Gang San <80745404+River-Mt@users.noreply.github.com>
2023-06-27 22:16:18 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.cc
@@ -0,0 +1,48 @@
+#include <cstdio>
+#include <algorithm>
+#include <deque>
+#include <vector>
+#include <iostream>
+using namespace std;
+typedef pair<int, int> pi;
+int n;
+deque<pi> dq;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n;
+    
+    for(int i=1;i<=n;++i) {
+        int move;
+        cin >> move;
+        dq.push_back({i, move});
+    }
+    
+    while(!dq.empty()) {
+        int move = dq.front().second;
+        cout << dq.front().first << ' ';
+        dq.pop_front();
+        
+        if(move < 0) {
+            move *= -1;
+            while(move--) {
+                dq.push_front(dq.back());
+                dq.pop_back();
+            }
+            
+        }
+        
+        else {
+            move -= 1;
+            while(move--) {
+                dq.push_back(dq.front());
+                dq.pop_front();
+            }
+        }
+        
+    }
+    
+    return 0;
+}
