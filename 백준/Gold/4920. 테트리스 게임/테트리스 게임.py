09232858602086e1663944e3273e952a8a97c0be
Kim Gang San <80745404+River-Mt@users.noreply.github.com>
2023-07-11 20:06:54 +0900

    [Gold V] Title: 테트리스 게임, Time: 356 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/4920. 테트리스 게임/테트리스 게임.py
@@ -0,0 +1,123 @@
+import sys
+input = sys.stdin.readline
+
+shape1 = [
+    [(0, 0), (0, 1), (0, 2), (0, 3)],
+    [(0, 0), (1, 0), (2, 0), (3, 0)]
+]
+
+shape2 = [
+    [(0, 0), (0, 1), (1, 1), (1, 2)],
+    [(0, 0), (1, 0), (1, -1), (2, -1)],
+]
+
+shape3 = [
+    [(0, 0), (0, 1), (0, 2), (1, 2)],
+    [(0, 0), (0, 1), (-1, 1), (-2, 1)],
+    [(0, 0), (1, 0), (1, 1), (1, 2)],
+    [(0, 0), (1, 0), (2, 0), (0, 1)]
+]
+
+shape4 = [
+    [(0, 0), (0, 1), (0, 2), (1, 1)],
+    [(0, 0), (-1, 1), (0, 1), (0, 2)],
+    [(0, 0), (1, 0), (2, 0), (1, 1)],
+    [(0, 0), (1, 0), (2, 0), (1, -1)]
+]
+
+shape5 = [
+    [(0, 0), (0, 1), (1, 0), (1, 1)]
+]
+
+def is_over(r, c, n):
+    if r < 0 or c < 0 or r >= n or c >= n:
+        return True
+    else:
+        return False
+
+
+def get_shape(arr, r, c):
+    ret = -4000000
+
+    for shape in shape1:
+        tmp = 0
+        for (dr, dc) in shape:
+            nr = r + dr
+            nc = c + dc
+            if is_over(nr, nc, n):
+                tmp = -4000000
+                break
+            tmp += arr[nr][nc]
+        ret = max(ret, tmp)
+
+
+    for shape in shape2:
+        tmp = 0
+        for (dr, dc) in shape:
+            nr = r + dr
+            nc = c + dc
+            if is_over(nr, nc, n):
+                tmp = -4000000
+                break
+            tmp += arr[nr][nc]
+        ret = max(ret, tmp)
+
+
+    for shape in shape3:
+        tmp = 0
+        for (dr, dc) in shape:
+            nr = r + dr
+            nc = c + dc
+            if is_over(nr, nc, n):
+                tmp = -4000000
+                break
+            tmp += arr[nr][nc]
+        ret = max(ret, tmp)
+
+
+    for shape in shape4:
+        tmp = 0
+        for (dr, dc) in shape:
+            nr = r + dr
+            nc = c + dc
+            if is_over(nr, nc, n):
+                tmp = -4000000
+                break
+            tmp += arr[nr][nc]
+        ret = max(ret, tmp)
+
+
+    for shape in shape5:
+        tmp = 0
+        for (dr, dc) in shape:
+            nr = r + dr
+            nc = c + dc
+            if is_over(nr, nc, n):
+                tmp = -4000000
+                break
+            tmp += arr[nr][nc]
+        ret = max(ret, tmp)
+
+
+    return ret
+
+
+def get_max(arr, n):
+    ret = -4000000
+    for i in range(n):
+        for j in range(n):
+            ret = max(ret, get_shape(arr, i, j))
+    return ret
+
+
+idx = 1
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    arr = [list(map(int, input().split())) for _ in range(n)]
+    print(f'{idx}. {get_max(arr, n)}')
+    idx += 1
+
+
