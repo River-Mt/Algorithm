61152e179a76935429bb548694932f3e1039437a
Kim Gang San <80745404+River-Mt@users.noreply.github.com>
2023-08-08 19:35:00 +0900

    [level 0] Title: 배열의 유사도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.py
@@ -1,9 +1,2 @@
 def solution(s1, s2):
-    answer = 0
-    dic = {}
-    for s in s1:
-        dic[s] = 1
-    for s in s2:
-        if dic.get(s):
-            answer += 1
-    return answer
+    return len(set(s1).intersection(set(s2)))
