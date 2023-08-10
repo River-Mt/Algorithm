c64ed679abf3c0819e195b86af22bd8c92de8a24
Kim Gang San <80745404+River-Mt@users.noreply.github.com>
2023-08-10 16:52:26 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 504 ms, Memory: 175948 KB -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -0,0 +1,26 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def get_alpha_ord(alp):
+    return ord(alp) - ord('a')
+
+
+s = input().rstrip()
+q = int(input())
+p_sum = [[0 for _ in range(27)] for _ in range(len(s) + 1)]
+
+for i in range(1, len(s) + 1):
+    alp_ord = get_alpha_ord(s[i - 1])
+
+    for j in range(27):
+        p_sum[i][j] = p_sum[i-1][j]
+
+    p_sum[i][alp_ord] += 1
+
+for i in range(q):
+    ch, l, r = input().split()
+    alp_ord = ord(ch) - ord('a')
+    l, r = int(l) + 1, int(r) + 1
+    print(p_sum[r][alp_ord] - p_sum[l - 1][alp_ord])
