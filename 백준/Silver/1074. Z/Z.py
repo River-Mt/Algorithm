88f1c9a86eb33ed07c87eb6e5ab71eaa14a0fddd
Kim Gang San <80745404+River-Mt@users.noreply.github.com>
2023-08-24 12:57:13 +0900

    [Silver I] Title: Z, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -1,19 +1,23 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 sys.setrecursionlimit(10 ** 8)
 num = 0
-ans = 0
 
 
 def solve():
     n, r, c = map(int, input().split())
     edge = 2 ** n
 
+    def is_exit(cur_r, cur_c):
+        return cur_r == r and cur_c == c
+
+    def can_skip(cur_len, cur_r, cur_c):
+        return not (cur_r <= r < cur_r + cur_len and cur_c <= c < cur_c + cur_len)
+
     def recur(cur_len, cur_r, cur_c):
-        global num, ans
-        if cur_r == r and cur_c == c:
+        global num
+        if is_exit(cur_r, cur_c):
             print(num)
             exit(0)
 
@@ -21,11 +25,12 @@ def solve():
             num += 1
             return
 
-        if not (cur_r <= r < cur_r + cur_len and cur_c <= c < cur_c + cur_len):
+        if can_skip(cur_len, cur_r, cur_c):
             num += cur_len ** 2
             return
 
         half = cur_len // 2
+
         recur(half, cur_r, cur_c)
         recur(half, cur_r, cur_c + half)
         recur(half, cur_r + half, cur_c)
