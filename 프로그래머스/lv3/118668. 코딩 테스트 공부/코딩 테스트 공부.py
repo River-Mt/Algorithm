276fdc2d005f9863d2dcd9776e0cf95d5623c941
Kim Gang San <80745404+River-Mt@users.noreply.github.com>
2023-09-05 22:10:12 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 4.72 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
+++ 프로그래머스/lv3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
@@ -1,30 +1,46 @@
+import heapq
+import sys
 def solution(alp, cop, problems):
-    alp_max, cop_max = [0, 0]
-
-    for problem in problems:
-        alp_max = max(alp_max, problem[0])
-        cop_max = max(cop_max, problem[1])
-
-    dp = [[100 * 300] * (cop_max + 1) for _ in range(alp_max + 1)]
-
-    alp = min(alp, alp_max)
-    cop = min(cop, cop_max)
-
-    dp[alp][cop] = 0
-
-    for i in range(alp, alp_max + 1):
-        for j in range(cop, cop_max + 1):
-            if i < alp_max:
-                dp[i + 1][j] = min(dp[i + 1][j], dp[i][j] + 1)
-
-            if j < cop_max:
-                dp[i][j + 1] = min(dp[i][j + 1], dp[i][j] + 1)
-
-            for alp_req, cop_req, alp_rwd, cop_rwd, cost in problems:
-                if i < alp_req or j < cop_req:
-                    continue
-                next_alp = min(i + alp_rwd, alp_max)
-                next_cop = min(j + cop_rwd, cop_max)
-                dp[next_alp][next_cop] = min(dp[next_alp][next_cop], dp[i][j] + cost)
-
-    return dp[alp_max][cop_max]
+    answer = 0
+    pq = []
+    
+    problems.append([1, 0, 1, 0, 1])
+    problems.append([0, 1, 0, 1, 1])
+    
+    problems.sort()
+    
+    alp_max, cop_max = 0, 0
+    
+    for p in problems:
+        alp_max = max(alp_max, p[0])
+        cop_max = max(cop_max, p[1])
+    
+    dist = [[300 * 100 for _ in range(cop_max + 1)] for _ in range(alp_max + 1)]
+    
+    alp, cop = min(alp, alp_max) , min(cop, cop_max)
+    dist[alp][cop] = 0
+    
+    heapq.heappush(pq, [0, alp, cop])
+    
+    while pq:
+        t, a, c = heapq.heappop(pq)
+        
+        if dist[a][c] < t:
+            continue
+        
+        for alp_req, cop_req, alp_rwd, cop_rwd, cost in problems:
+            if a < alp_req or c < cop_req:
+                continue
+                
+            if a < alp_req and c < cop_req:
+                break
+            
+            na = min(alp_max, a + alp_rwd)
+            nc = min(cop_max, c + cop_rwd)
+            
+            if dist[na][nc] > dist[a][c] + cost:
+                dist[na][nc] = dist[a][c] + cost
+                heapq.heappush(pq, [dist[na][nc], na, nc])
+        
+    
+    return dist[alp_max][cop_max]
