d03fb8ddc8d22a51e74a7d7212a676b7e8a6c99a
Kim Gang San <80745404+River-Mt@users.noreply.github.com>
2023-09-28 14:10:33 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -0,0 +1,12 @@
+def solution(citations):
+    answer = 0
+    citations.sort()
+    n = len(citations)
+
+    for i in range(n):
+        h = n-i
+        if citations[i] >= h:
+            answer = h
+            break
+    
+    return answer
