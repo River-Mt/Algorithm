2e0a967e5245015defccee2203152765ecf747e6
Kim Gang San <80745404+River-Mt@users.noreply.github.com>
2023-10-23 16:14:43 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 100 ms, Memory: 33860 KB -BaekjoonHub

--- 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
@@ -1,25 +1,18 @@
 import sys
 
 input = sys.stdin.readline
-
 n = int(input())
-buildings = [int(input()) for _ in range(n)]
 st = []
 ans = 0
 
-for i, h in enumerate(buildings):
-    if not len(st):
-        st.append([h, i])
-    else:
-        while st:
-            top_h, top_idx = st[-1]
-            if top_h > h:
-                break
-            ans += i - top_idx - 1
-            st.pop()
-        st.append([h, i])
-
-for i in range(len(st) - 1):
-    ans += st[-1][1] - st[i][1]
+for i in range(n):
+    h = int(input())
+    while st and st[-1] <= h:
+        st.pop()
+    ans += len(st)
+    st.append(h)
 
 print(ans)
+
+
+
