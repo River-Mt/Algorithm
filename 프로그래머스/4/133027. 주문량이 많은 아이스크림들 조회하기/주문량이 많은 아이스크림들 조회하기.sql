31240d4bd85b90669cd857579b43ec045f8962ef
Kim Gang San <80745404+River-Mt@users.noreply.github.com>
2024-03-17 13:34:24 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,14 +1,5 @@
-
-
-
-select flavor
-from (
-    select
-        a.flavor
-    ,   sum(a.total_order + b.total_order) sum
-    from first_half a, july b
-    where a.flavor = b.flavor
-    group by a.flavor
-    order by sum desc
-)
-where rownum <= 3
+select a.flavor
+from first_half a, july b
+where a.flavor = b.flavor
+group by a.flavor
+order by sum(a.total_order + b.total_order) desc fetch next 3 rows only
